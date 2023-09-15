e060187682568d49f5cf86730de78dc3e09e33ae
Krishnarjun Nanda <krishnarjunnanda@gmail.com>
2023-09-15 12:31:36 +0530

    Revert "Develop"

--- components/Testimonial.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Testimonial() {
-  return (
-    <div id='testimonial' className='bg-blue-500 h-screen w-screen pt-20'>
-        Testimonial
-    </div>
-  )
-}
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+'use-client'
+import { Input } from '../../lib/chakraui';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+      hello
+      <Input/>
+    </div>
+  );
+}
