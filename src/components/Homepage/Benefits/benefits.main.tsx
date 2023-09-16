4a5da16e94adcf983e8eb59d3a27018f1f6a5252
Ekam-Bitt <ekambitt@gmail.com>
2023-09-17 04:01:24 +0530

    responsive

--- src/components/Homepage/Benefits/benefits.main.tsx
+++ src/components/Homepage/Benefits/benefits.main.tsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import CustomTabs from './benefits.card'
+import React from 'react';
+import CustomTabs from './benefits.card';
 export default function Benefits() {
-    return (
-        <div id="benefits" className="bg-[#15B9FF] w-screen py-24">
-            <section>
-                <h1 className="text-3xl pl-8">Benefits</h1>
-                <div className=" flex flex-col-reverse gap-6 lg:flex-row flex-wrap items-start max-w-full m-auto py-12">
-                    <div className="md:w-[67vw] md:mb-6 lg:w-[50vw] mb-12 pr-12 md:pr-0">
-                        <img src="/skin.svg" alt="skin" className='sm:pr-8'/>
-                    </div>
-                    <div className=" w-full m-auto lg:m-0 md:w-[67%] lg:w-[40%] lg:ml-20">
-                      < CustomTabs/>
-                    </div>
-                </div>
-            </section>
+  return (
+    <div id="benefits" className="bg-[#15B9FF] w-screen pt-24">
+      <h1 className="text-3xl pl-8">Benefits</h1>
+      <div className="relative inset-0">
+        <div className="flex flex-col-reverse gap-4 lg:flex-row flex-wrap items-start max-w-full m-auto py-12">
+          <div className="md:w-[67vw] md:mb-6 lg:w-[50vw] mb-12 pr-12 md:pr-0">
+            <img src="/skin.svg" alt="skin" className="sm:pr-8" />
+          </div>
+          <div className=" w-full m-auto lg:m-0 md:w-[67%] lg:w-[40%] lg:ml-20">
+            <CustomTabs />
+          </div>
         </div>
-    )
+        <h1 className=" absolute inset-0 z-30 top-[950px] lg:top-[500px] w-full text-2xl md:text-4xl lg:text-7xl text-center justify-center flex items-center text-white">
+          The Future of Dermatology
+          <br />
+          at your finger tips...
+        </h1>
+      </div>
+      <div className=" w-screen h-[vw] bg-gradient-to-b from-[#15B9FF] to-[#76D6FF]">
+        <img src="/Ellipse.png" alt="Gradient circle" className='w-screen'/>
+      </div>
+    </div>
+  );
 }
--- src/components/Homepage/Hero.main.tsx
+++ src/components/Homepage/Hero.main.tsx
@@ -1,18 +1,23 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react'
+import React from 'react';
 
 const Hero = () => {
-    return (
-        <div id="home" className="relative -mt-20 h-screen">
-            <img src="/hero.png" className="absolute inset-0 object-cover w-full h-full" />
-            <section className="absolute inset-0 flex flex-col justify-center items-center text-center gap-3">
-                <h1 className="text-white text-3xl md:text-6xl leading-[5rem] font-holtwood">DERMACURE.AI</h1>
-                <h2 className="text-white text-lg font-bold">
-                    YOUR ONE STOP AI ASSISTANT FOR ALL DERMATOLOGICAL PROBLEMS
-                </h2>
-            </section>
-        </div>
-    )
-}
+  return (
+    <div id="home" className="relative -mt-20 h-screen">
+      <img
+        src="/hero.png"
+        className="absolute inset-0 object-cover w-full h-full"
+      />
+      <section className="absolute inset-0 flex flex-col justify-center items-center text-center gap-3">
+        <h1 className="text-white text-lg sm:text-3xl md:text-6xl leading-[5rem] font-holtwood">
+          DERMACURE.AI
+        </h1>
+        <h2 className="text-white text-lg font-bold">
+          YOUR ONE STOP AI ASSISTANT FOR ALL DERMATOLOGICAL PROBLEMS
+        </h2>
+      </section>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
