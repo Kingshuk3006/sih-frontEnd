2abb7ea6df54362470e7c9803d7ed10685d7853d
Kinghshuk Sarkar <kinghshuksarkar@Kinghshuks-MacBook-Air.local>
2023-09-16 13:38:17 +0530

    ui merge conflict resolved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
-'use-client'
-import { Input } from '../../lib/chakraui';
-import Image from 'next/image';
+'use-client';
+
+import Benefits from "@/components/Homepage/Benefits/benefits.main";
+import Hero from "@/components/Homepage/Hero.main";
+import Navbar from "@/components/Homepage/Navbar.main";
+import Testimonial from "@/components/Homepage/Testimonial/Testimonial.main";
 
 export default function Home() {
+
   return (
-    <div>
-      hello
-      <Input/>
+    <div className="flex min-h-screen flex-col bg-blueBackground">
+      <Navbar />
+      <Hero />
+      <Benefits />
+      <Testimonial />
+      {/* <ContactUs /> */}
     </div>
-  );
+  )
 }
---
+++ src/components/Homepage/Benefits/benefits.main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import CustomTabs from './benefits.card'
+export default function Benefits() {
+    return (
+        <div id="benefits" className="bg-[#15B9FF] w-screen py-24">
+            <section>
+                <h1 className="text-3xl pl-8">Benefits</h1>
+                <div className=" flex flex-col-reverse gap-6 lg:flex-row flex-wrap items-start max-w-full m-auto py-12">
+                    <div className="md:w-[67%] md:mb-6 lg:w-[50%] mb-12">
+                        <img src="/skin.svg" alt="skin" className='sm:pr-8'/>
+                    </div>
+                    <div className=" w-full m-auto lg:m-0 md:w-[67%] lg:w-[40%] lg:ml-20">
+                      < CustomTabs/>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
