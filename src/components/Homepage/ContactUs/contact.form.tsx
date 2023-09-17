9f57a1c3b9a44053bd33f3c1f2fd0b3753b809c0
Ekam-Bitt <ekambitt@gmail.com>
2023-09-18 02:16:33 +0530

    Contact us completed

--- src/components/Homepage/ContactUs/contact.form.tsx
+++ src/components/Homepage/ContactUs/contact.form.tsx
@@ -6,24 +6,58 @@ import {
   FormControl,
   FormErrorMessage,
   FormLabel,
-  Input
-} from '../../../../lib/chakraui'; // Import individual Chakra UI components
+  Input,
+  Textarea // Import Textarea for Query Text/Feedback
+} from '../../../../lib/chakraui';
 import { Field, Form, Formik } from 'formik';
 
 export default function ContactForm() {
+  // Validation function for Name
   function validateName(value) {
     let error;
     if (!value) {
-      error = 'Name is required';
+      error = 'Name is Required';
+    }
+    return error;
+  }
+
+  // Validation function for Email
+  function validateEmail(value) {
+    let error;
+    if (!value) {
+      error = 'Email is Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+      error = 'Invalid email address';
+    }
+    return error;
+  }
+
+  // Validation function for Phone Number
+  function validatePhone(value) {
+    let error;
+    if (!value) {
+      error = 'Phone Number is Required';
+    } else if (!/^\d{10}$/i.test(value)) {
+      error = 'Invalid phone number';
+    }
+    return error;
+  }
+
+  // Validation function for Query
+  function validateQuery(value) {
+    let error;
+    if (!value) {
+      error = 'Feedback is Required';
     }
     return error;
   }
 
   return (
     <Formik
-      initialValues={{ name: '' }}
+      initialValues={{ name: '', email: '', phone: '', query: '' }} // Add email, phone, and query fields
       onSubmit={(values, actions) => {
         setTimeout(() => {
+          // Display the form values in an alert
           alert(JSON.stringify(values, null, 2));
           actions.setSubmitting(false);
         }, 1000);
@@ -36,7 +70,6 @@ export default function ContactForm() {
               <FormControl
                 isInvalid={form.errors.name && form.touched.name}
                 mb={4}
-                className=""
               >
                 <div className="flex justify-between items-center">
                   <FormLabel className="text-center text-xl m-0 p-0">
@@ -45,11 +78,70 @@ export default function ContactForm() {
                   <Input
                     {...field}
                     id="name"
-                    className=" w-[500px] bg-white border border-black hover:border-black"
+                    className="w-[500px] bg-white border border-black hover:border-black"
                   />
                 </div>
-
                 <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+          <Field name="email" validate={validateEmail}>
+            {({ field, form }) => (
+              <FormControl
+                isInvalid={form.errors.email && form.touched.email}
+                mb={4}
+              >
+                <div className="flex justify-between items-center">
+                  <FormLabel className="text-center text-xl m-0 p-0">
+                    Email
+                  </FormLabel>
+                  <Input
+                    {...field}
+                    id="email"
+                    type="email" // Set input type to 'email'
+                    className="w-[500px] bg-white border border-black hover:border-black"
+                  />
+                </div>
+                <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+          <Field name="phone" validate={validatePhone}>
+            {({ field, form }) => (
+              <FormControl
+                isInvalid={form.errors.phone && form.touched.phone}
+                mb={4}
+              >
+                <div className="flex justify-between items-center">
+                  <FormLabel className="text-center text-xl m-0 p-0">
+                    Phone Number
+                  </FormLabel>
+                  <Input
+                    {...field}
+                    id="phone"
+                    type="tel" // Set input type to 'tel' for phone numbers
+                    className="w-[500px] bg-white border border-black hover:border-black"
+                  />
+                </div>
+                <FormErrorMessage>{form.errors.phone}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+          <Field name="query" validate={validateQuery}>
+            {({ field, form }) => (
+              <FormControl
+                isInvalid={form.errors.query && form.touched.query}
+                mb={4}
+              >
+                <div>
+                  <FormLabel className="text-xl m-0 p-0">Feedback</FormLabel>
+                  <Textarea
+                    {...field}
+                    id="query"
+                    className="w-full h-40 m-0 p-4 bg-white border border-black hover:border-black"
+                  />
+                </div>
+                <FormErrorMessage>{form.errors.query}</FormErrorMessage>
               </FormControl>
             )}
           </Field>
@@ -59,7 +151,7 @@ export default function ContactForm() {
             isLoading={props.isSubmitting}
             type="submit"
           >
-            Schedule Consult
+            Submit
           </Button>
         </Form>
       )}
