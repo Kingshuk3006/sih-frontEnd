48f00ed133d2f25481594e9bdbd2188471c1dec1
Ekam-Bitt <ekambitt@gmail.com>
2023-09-18 15:47:07 +0530

    e clinic

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,14 +2,14 @@
 
 import BotsonicWidget from '@/components/Chatbot/Chatbot.main';
 import Navbar from '@/components/Header/Navbar.main';
-import Model from '@/components/Model/model.main';
+
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col bg-blueBackground">
       <Navbar />
       <BotsonicWidget />
-      <Model />
+      
     </div>
   );
 }
